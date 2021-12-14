759593b811e0ae9a6a27ff38d04c6d42b7e21a0a
Autr <gilbert.sinnott@gmail.com>
2021-12-14 14:22:23 +0100

    sorting change

--- src/defaults.js
+++ src/defaults.js
@@ -6,6 +6,17 @@ function log( msg ) {
 
 let SORT_TIMESTAMP
 
+window.addEventListener( 'mousedown', e => {
+	// const CP = SORT_TIMESTAMP
+	// SORT_TIMESTAMP = -999
+	// setTimeout( e => {
+	// 	console.log(SORT_TIMESTAMP, '???')
+	// 	if (SORT_TIMESTAMP != -999) {
+	// 		SORT_TIMESTAMP = CP
+	// 	}
+	// }, 200)
+})
+
 export const defaults = {
 	hover: o => log(`${o.id} "${o.key}" -> hovered`),
 	click: o => log(`${o.id} "${o.key}" -> clicked`),
@@ -16,9 +27,16 @@ export const defaults = {
 		let timestamp = (new Date() * 1)
 		SORT_TIMESTAMP = timestamp
 		let copy = [...source]
+		const END = ts => {
+			if (ts != SORT_TIMESTAMP) {
+				console.log(`[svelte-tabular-table] end sort loop: ${SORT_TIMESTAMP}`)
+				return true	
+			}
+			return false
+		}
 		window.requestAnimationFrame( e => {
 
-			if ( timestamp != SORT_TIMESTAMP ) return
+			if ( END(timestamp) ) return
 
 			let idx = 0
 			copy = sort( copy )[ direction ? 'asc' : 'desc' ]( [
@@ -26,22 +44,30 @@ export const defaults = {
 				u => u.updated
 			])
 
+			return callback( copy )
+			
 			const min = 20
 			let increase = 1
 
 			function sendCallback() {
-				if ( timestamp != SORT_TIMESTAMP ) return
+				if ( END(timestamp) ) return
 				let neu = []
 				for (let i = 0; i < increase; i++ ) {
 					if (copy[idx]) neu.push( copy[idx] )
 					idx += 1
 				}
-				if ( idx > min ) increase += 1
+				if ( idx > min ) increase += 2
 				callback( neu )
-				if (idx < copy.length) window.requestAnimationFrame( sendCallback )
+				if (idx < copy.length) {
+					console.log('req')
+					window.requestAnimationFrame( sendCallback )
+				} else {
+					console.log(`[svelte-tabular-table] finished sending sorted: ${copy.length}`)
+
+				}
 			}
 
-			if ( timestamp != SORT_TIMESTAMP ) return
+			if ( END(timestamp) ) return
 			sendCallback()
 		})
 	},
